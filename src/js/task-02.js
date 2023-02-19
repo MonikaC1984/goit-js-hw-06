5941b7fd6e52053fafbcf449cf11a70b1672ce27
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-02-20 00:15:44 +0100

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const tab = document.querySelector('#ingredients');
+const list = [];
+
+//dodac li do nowej tablicy na pozycji x
+//pobrac i dodac tresc do li z indeksem x
+//dodac klase “item” do li z indeksem x
+
+for (let i = 0; i < ingredients.length; i++) {
+  list[i] = document.createElement('li');
+  list[i].textContent = ingredients[i];
+  list[i].classList.add('item');
+  console.log(list);
+  tab.append(list[i]);
+}
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,5 @@
+const tab = document.querySelector('.gallery');
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,7 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const markup = images.map(img => `<li>${img}</li>`).join('');
+
+tab.insertAdjacentHTML('afterbegin', markup);
