63373db561f57f910b3ff1599f358a8c89d8d750
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-02-21 15:59:58 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', event => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  text.textContent = randomColor;
+});
